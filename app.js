2bdbadf5ed27a0f32661681885332f6069bd9ea1
Dasannn <86085665+Dasannn@users.noreply.github.com>
2021-06-17 19:26:57 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var nombredelusuario = "cris";
+let lastname = "archila"
+
+nombredelusuario ="gordo";
+
+ // hola xd
+ 
+const PI = 3.141627
+const id = "javier"
+console.log (PI, id)
+
+
+
+
+
---
+++ prueba.js
@@ -0,0 +1,10 @@
+var cris = 50
+var javier = 61
+
+if (cris + javier === 110) {
+    alert ("puto")
+
+} else {
+    alert ("pendejito")
+}
+
