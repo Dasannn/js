3f73b733eef36674774df01124bb5c294742f080
Dasannn <86085665+Dasannn@users.noreply.github.com>
2021-06-17 21:14:31 -0500

    main

--- prueba.js
+++ prueba.js
@@ -1,6 +1,6 @@
 var count = 10000
 
 while (count > 0) {
-    document.write("<li>Pasale esta pagina a tu amigo el furro</li>")
+    document.write("<li>Pasale esta pagina a tu amigo el furro =v</li>")
     count = count - 1
 }
